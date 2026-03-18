aac7cccff29cbcdc8f2f6867374c67d8cdab4d77
AghakanJR <nreaich20@gmail.com>
2026-03-18 17:55:24 +0000

    Import Directly your Data within the Github Code

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-st.title("CSV File Uploader")
+st.title("CSV File Viewer")
 
-uploaded_file = st.file_uploader ("Upload a CSV File", type=["csv"])
-
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    st.write("### Preview of Uploaded Data:")
-    st.dataframe(df.head())
+df = pd.read_csv("Datasets/trips_data_1000.csv")
+st.write("Preview Uploaded Data:")
+st.dataframe(df.head())
